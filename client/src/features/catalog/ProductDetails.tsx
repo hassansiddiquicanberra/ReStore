6613b970b1e5a091e50cd12d6329e26be4cd4c98
Hassan <hassan.siddiqui@live.com.au>
2022-07-07 17:11:47 +1000

    style details

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,12 @@
-import { Typography } from "@mui/material";
+import {
+  Divider,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -22,5 +30,46 @@ export default function ProductDetails() {
 
   if (!product) return <h3>Product not found!</h3>;
 
-  return <Typography variant="h2">{product.name}</Typography>;
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={6}>
+        <img
+          src={product?.pictureUrl}
+          alt={product?.name}
+          style={{ width: "100%" }}
+        ></img>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 3 }}></Divider>
+        <Typography variant="h4">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>{product.name}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Description</TableCell>
+              <TableCell>{product.description}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Type</TableCell>
+              <TableCell>{product.type}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Brand</TableCell>
+              <TableCell>{product.brand}</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Quantity in Stock</TableCell>
+              <TableCell>{product.quantityInStock}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Grid>
+    </Grid>
+  );
 }
